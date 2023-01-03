fc9a705fa50c259c1adc16db5e74085aab94e23d
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-03 08:33:48 -0800

    Feat: create #recommend_exhibits(patron)

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,12 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits.push(exhibit)
   end
+
+  def recommend_exhibits(patron)
+    patron.interests.flat_map do |interest|
+      exhibits.select do |exhibit|
+        exhibit.name == interest
+      end
+    end
+  end
 end
