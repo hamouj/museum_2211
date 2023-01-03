6e2d44ff8a869657036c94c075e944bd688f6d6d
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-03 08:43:53 -0800

    Test: create test for #patrons_by_exhibit_interest

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -67,4 +67,25 @@ describe Museum do
       expect(dmns.patrons).to eq([patron_1, patron_2, patron_3])
     end
   end
+
+  describe '#patrons_by_exhibit_interest' do
+    before(:each) do
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_2.add_interest("Dead Sea Scrolls")
+      patron_3.add_interest("Dead Sea Scrolls")
+    end
+
+    it 'creates a hash of each exhibit and interested patrons' do
+      expected_hash = {
+        gems_and_minerals => [patron_1]
+        dead_sea_scrolls => [patron_1, patron_2, patron_3]
+        imax => []
+      }
+      expect(dmns.patrons_by_exhibit_interest).to eq(expected_hash)
+    end
+  end
 end
